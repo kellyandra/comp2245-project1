b23e42ef08e973e287f7324da7957aacee3e93e9
Kelly Christian <ms.kchristian86@gmail.com>
2022-11-02 15:58:51 -0400

    not working java script file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+let email;
+let button= document.getElementsByClassName ('btn');
+let message= document.getElementsByClassName('Message');
+
+document.getElementsByClassName(btn).onclick = function(){
+    email=document.getElementById("email");
+    console.log(email);
+}
+
+
+
+/*function mfunction(){
+    message.innerHTML=`Thank you! Your email address ${email} has been added to our mailing list! `;
+}
+*/
